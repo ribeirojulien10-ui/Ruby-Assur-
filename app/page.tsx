7188fba6954fdf245051e13f6766ded4032f9cc9
Ribeiro Antoine <antoine@macbook-pro-de-ribeiro1.home>
2026-06-09 19:54:41 +0200

    Hero : réduit padding-top, contenu collé en haut

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <>
       {/* Hero */}
-      <section className="relative isolate flex min-h-[calc(100svh-64px)] items-start overflow-hidden bg-navy pt-16 pb-20 text-white sm:pt-24">
+      <section className="relative isolate flex min-h-[calc(100svh-64px)] items-start overflow-hidden bg-navy pt-8 pb-20 text-white sm:pt-12">
         <Image
           src="/images/hero-ruby-assur.png"
           alt="Courtier en assurance Ruby Assur'"
